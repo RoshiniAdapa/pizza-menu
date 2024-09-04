b718a384506d3961a131783b78efddf7ee98e1fb
Roshini Adapa <roshiniadapa@gmail.com>
2024-09-04 23:09:15 +0530

    Pizza Menu | Destructuring props

--- src/index.js
+++ src/index.js
@@ -116,16 +116,16 @@ function Menu() {
   );
 }
 
-function Pizza(props) {
-  if (props.pizzaObj.soldOut) return null;
+function Pizza({ pizzaObj }) {
+  if (pizzaObj.soldOut) return null;
 
   return (
     <li className="pizza">
-      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
-        <h3>{props.pizzaObj.name}</h3>
-        <p>{props.pizzaObj.ingredients}</p>
-        <span>{props.pizzaObj.price + 3}</span>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <span>{pizzaObj.price + 3}</span>
       </div>
     </li>
   );
@@ -155,10 +155,12 @@ function Footer() {
   //   return React.createElement("footer", null, "We're currently open");
 }
 
-function Order(props) {
+function Order({ openHour, closeHour }) {
   return (
     <div className="order">
-      <p>We're open until {props.closeHour}:00. Come visit us or order</p>
+      <p>
+        We're open from {openHour}:00 to {closeHour}:00. Come visit us or order
+      </p>
       <button className="btn">Order</button>
     </div>
   );
