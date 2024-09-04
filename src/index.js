495663ad77524307c83c92668d1a34800b68e280
Roshini Adapa <roshiniadapa@gmail.com>
2024-09-04 23:16:03 +0530

    Pizza Menu | React Fragments

--- src/index.js
+++ src/index.js
@@ -90,11 +90,17 @@ function Menu() {
 
       {/* Method2 */}
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzaData.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic italian cuisine, 6 creative dishes to choose from. All
+            from our store oven, all organic, all delicious
+          </p>
+          <ul className="pizzas">
+            {pizzaData.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We're still working on our menu. Please come back later</p>
       )}
